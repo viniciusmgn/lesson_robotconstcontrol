08da6e9180f39c7be131029c837bc1bbe1471131
viniciusmgn <vinicius.marianog@gmail.com>
2025-05-22 21:28:36 -0300

    case study 1 started

--- presentation/code/part3/anim_part_3_1.py
+++ presentation/code/part3/anim_part_3_1.py
@@ -3,15 +3,16 @@ import uaibot as ub
 import matplotlib.pyplot as plt
 
 #Parameters
-param_eta = 0.4*3
+param_eta = 1.2
 param_eps = 0.001
-param_k = 0.5*2
+param_k = 1.0
 param_max_qdot = 1.5
 param_obs_delta = 0.025 
 param_joint_delta = 2*np.pi/180
 dt=0.005
 param_iter_max = 10000
 robot = ub.Robot.create_kuka_lbr_iiwa(htm=ub.Utils.rotz(np.pi/2))
+param_use_pc = False
 
 htm_tg_0 = ub.Utils.trn([0.3, 0.0, 0.64])*ub.Utils.roty(np.pi)
 htm_tg_1 = ub.Utils.trn([0.3, 0.0, 0.54])*ub.Utils.roty(np.pi)
@@ -21,9 +22,6 @@ htm_tg_4 = ub.Utils.trn([-0.65, 0.0, 0.57])*ub.Utils.roty(np.pi)
 htm_tg_5 = ub.Utils.trn([-0.65, 0.0, 0.63])*ub.Utils.roty(np.pi)
 htm_tg_6 = ub.Utils.trn([-0.35, 0.35, 0.77])*ub.Utils.roty(-np.pi/2)
 htm_tg_7 = ub.Utils.trn([0.30,  0.20, 0.64])*ub.Utils.roty(np.pi)
-
-
-
 
 #####################
 #Create environment
@@ -40,7 +38,7 @@ texture_gold = ub.Texture(
 material_steel= ub.MeshMaterial(metalness=0.7, clearcoat=1, roughness=0.5, normal_scale=[0.5, 0.5], texture_map=texture_steel)
 material_gold= ub.MeshMaterial(metalness=0.7, clearcoat=1, roughness=0.5, normal_scale=[0.5, 0.5], texture_map=texture_gold)
 
-platform1 = ub.Box(htm=ub.Utils.trn([0.3, 0.0, 0.25]),width=0.2,depth=3.0,height=0.5,mesh_material=material_steel)
+platform1 = ub.Box(htm=ub.Utils.trn([0.3, 0.0, 0.25]),width=0.2,depth=1.0,height=0.5,mesh_material=material_steel)
 platform2 = ub.Box(htm=ub.Utils.trn([-0.7, 0.0, 0.25]),width=0.8,depth=1.0,height=0.5,mesh_material=material_steel)
 platform3 = ub.Box(htm=ub.Utils.trn([-0.8, -0.35, 0.725]),width=0.6,depth=0.3,height=0.45,mesh_material=material_steel)
 platform4 = ub.Box(htm=ub.Utils.trn([-0.8, 0.35, 0.725]),width=0.6,depth=0.3,height=0.45,mesh_material=material_steel)
@@ -54,8 +52,9 @@ disk = ub.Cylinder(htm=ub.Utils.trn([0.3,0,0.52]),height=0.04,radius=0.05,mesh_m
 
 all_obstacles = [platform1, platform2, platform3, platform4, platform5, platform6, platform7]
 
-
-if True:
+#If point clouds are used, transform all objects in "all_obstalces" 
+#into point clouds
+if param_use_pc:
     
     all_points = []
     for obs in all_obstacles:
@@ -63,15 +62,14 @@ if True:
         
     all_obstacles = [ub.PointCloud(points=all_points, color='cyan', size=0.02)]
     
+#Create simulation and add everything
 sim = ub.Simulation.create_sim_factory([robot, button, disk])
+sim.set_parameters(load_screen_color="#191919", background_color="#191919", width=500, height=500, show_world_frame=False, show_grid=False, camera_start_pose=[1.0,-1.0,1.0,-1.0,0.8,0,0.8])
 
 sim.add(ub.Frame(htm_tg_0, size=0.1))
 sim.add(ub.Frame(htm_tg_3, size=0.1))
 sim.add(ub.Frame(htm_tg_6, size=0.1))
 sim.add(ub.Frame(htm_tg_7, size=0.1))
-
-
-
 
 for obs in all_obstacles:
     sim.add(obs)
@@ -143,11 +141,11 @@ def compute_control(_q, _htm_tg, is_handling_disk, care_obstacles):
     #Compute the control input
     u = ub.Utils.solve_qp(H, f, A, b)
 
-    return u, r
+    return u, np.linalg.norm(r)
         
 #######################
 
-
+sim.save("/home/vinicius/Desktop/Aulas/Robot Constrained Control/presentation/images/part3/","part_3_1")
 
 
 q = np.matrix(robot.q)
@@ -167,106 +165,105 @@ while cont:
     
     if mode==0:
         #Going slightly above the disk
-        u, r = compute_control(q, htm_tg_0, False, True)
+        u, error_r = compute_control(q, htm_tg_0, False, True)
         
-        print("Mode 0, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01:
+        print("Mode 0, error = "+str(round(error_r,3)))
+        if error_r<=0.01:
             mode = 1
 
     if mode==1:
         #Downward movement
-        u, r = compute_control(q, htm_tg_1, False, False)
+        u, error_r = compute_control(q, htm_tg_1, False, False)
         
-        print("Mode 1, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01:
+        print("Mode 1, error = "+str(round(error_r,3)))
+        if error_r<=0.01:
             mode = 2
             robot.attach_object(disk)
 
     if mode==2:
         #Upward movement
-        u, r = compute_control(q, htm_tg_2, False, False)
+        u, error_r = compute_control(q, htm_tg_2, False, False)
         
-        print("Mode 2, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01:
+        print("Mode 2, error = "+str(round(error_r,3)))
+        if error_r<=0.01:
             mode = 3
  
     if mode==3:
         #Move object to be delivered
-        u, r = compute_control(q, htm_tg_3, True, True)
+        u, error_r = compute_control(q, htm_tg_3, True, True)
     
-        print("Mode 3, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01: 
+        print("Mode 3, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 4
             
     if mode==4:
         #Move down
-        u, r = compute_control(q, htm_tg_4, True, False)
+        u, error_r = compute_control(q, htm_tg_4, True, False)
         
-        print("Mode 4, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01: 
+        print("Mode 4, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 5
             robot.detach_object(disk)
             
     if mode==5:
         #Move up
-        u, r = compute_control(q, htm_tg_5, False, False)
+        u, error_r = compute_control(q, htm_tg_5, False, False)
         
-        print("Mode 5, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01: 
+        print("Mode 5, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 6
                          
     if mode==6:
         #Go press the button
-        u, r = compute_control(q, htm_tg_6, False, True)
+        u, error_r = compute_control(q, htm_tg_6, False, True)
         
-        print("Mode 6, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.005:
+        print("Mode 6, error = "+str(round(error_r,3)))
+        if error_r<=0.005:
             mode = 7
 
     if mode==7:
         #Got the object again
-        u, r = compute_control(q, htm_tg_3, False, True)
+        u, error_r = compute_control(q, htm_tg_3, False, True)
         
-        print("Mode 7, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01: 
+        print("Mode 7, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 8
             
 
     if mode==8:
         #Go down
-        u, r = compute_control(q, htm_tg_4, False, False)
+        u, error_r = compute_control(q, htm_tg_4, False, False)
         
-        print("Mode 8, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.015: 
+        print("Mode 8, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 9
             robot.attach_object(disk)
             
     if mode==9:
         #Go up
-        u, r = compute_control(q, htm_tg_5, False, False)
+        u, error_r = compute_control(q, htm_tg_5, False, False)
         
-        print("Mode 9, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.015: 
+        print("Mode 9, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             mode = 10    
             
 
     if mode==10:
         #Move to final pose
-        u, r = compute_control(q, htm_tg_7, True, True)
+        u, error_r = compute_control(q, htm_tg_7, True, True)
         
-        print("Mode 10, error = "+str(round(np.linalg.norm(r),3)))
-        if np.linalg.norm(r)<=0.01: 
+        print("Mode 10, error = "+str(round(error_r,3)))
+        if error_r<=0.01: 
             cont = False 
                  
     hist_q.append(np.matrix(q))
     hist_u.append(np.matrix(u))
+    hist_t.append(i*dt)
                                        
     q = robot.q+u*dt
     
-    hist_t.append(i*dt)
     robot.add_ani_frame(time = i*dt, q = q)
-    
-    
+        
 plt.figure() 
 for i in range(7):
     
@@ -276,8 +273,7 @@ for i in range(7):
     plt.plot(hist_t, [robot.joint_limit[i,1]-param_joint_delta for j in range(len(hist_q))])
     plt.title("Joint "+str(i+1))
     plt.tight_layout()
-    # plt.figure()
-    # plt.plot([u[i,0] for u in hist_q])
+
  
 plt.figure()   
 for i in range(7):
@@ -291,4 +287,4 @@ for i in range(7):
         
 plt.show()
 
-sim.save()
+sim.save("/home/vinicius/Desktop/Aulas/Robot Constrained Control/presentation/images/part3/","part_3_2")
